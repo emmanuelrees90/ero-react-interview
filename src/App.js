e99471b9d16e530545707d85d4d26a055792ee30
Emmanuel Oppong <emmanuelroppong90@gmail.com>
2025-06-09 17:00:17 -0400

    update route path

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />}>
+                <Route path="/ero-react-interview" element={<Home />}>
                     <Route index element={<Home />} />
                 </Route>
                 <Route path="/table" element={<Table />} />
